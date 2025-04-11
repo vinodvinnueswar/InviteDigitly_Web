1a01452d273ba6632171351ad0f5a63cf9c66625
Sasispecious <sasispecious@gmail.com>
2025-04-10 23:20:55 -0700

    modified in packages

--- src/Components/Our_Collections.jsx
+++ src/Components/Our_Collections.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
 import WedInvite1 from '../images/Our_Collections/Wed_card1.png'
 import WedInvite2 from '../images/Our_Collections/Wed_Card2.png'
 import WedInvite3 from '../images/Our_Collections/Wed_Card3.png'
@@ -11,66 +12,114 @@ import HalfSaree1 from '../images/Our_Collections/Half_Saree.png'
 import HalfSaree2 from '../images/Our_Collections/Half_Saree1.png'
 import HalfSaree3 from '../images/Our_Collections/Half_Saree2.png'
 import HalfSaree4 from '../images/Our_Collections/Half_Saree3.png'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import { API_URL } from '../Pages/api';
 
 
 
 
 const Our_Collections = () => {
-    const DataList = [
-        {
-            img:WedInvite1
-        },
-        {
-            img:WedInvite2
-        },
-        {
-            img:WedInvite3
-        },
-        {
-            img:WedInvite4
-        },
-        {
-            img:BirthdayCard1
-        },
-        {
-            img:BirthdayCard2
-        },
-        {
-            img:BirthdayCard3
-        },
-        {
-            img:BirthdayCard4
-        },
-        {
-            img:HalfSaree1
-        },
-        {
-            img:HalfSaree2
-        },
-        {
-            img:HalfSaree3
-        },
-        {
-            img:HalfSaree4
-        },
+
+    const [vendorData, setVendorData] = useState([]);
+    const [products , setProducts] = useState([]);
+
+    // const {vendorId} = useParams();
+    // console.log(vendorId)
+
+    const vendorDataHandler = async()=>{
+        try {
+                const response = await fetch(`${API_URL}/vendor/all-vendors`)
+                const newData = await response.json()
+                    setVendorData(newData);
+                    console.log("this is api Data ", newData)
+                
+        } catch (error) {
+            alert("failed to fetch data")
+            console.error("failed to fetch data")
+          
+        }
+}
+        useEffect(()=>{
+            vendorDataHandler()
+        }, [])
+
+
+        
+
+    // const DataList = [
+    //     {
+    //         img:WedInvite1
+    //     },
+    //     {
+    //         img:WedInvite2
+    //     },
+    //     {
+    //         img:WedInvite3
+    //     },
+    //     {
+    //         img:WedInvite4
+    //     },
+    //     {
+    //         img:BirthdayCard1
+    //     },
+    //     {
+    //         img:BirthdayCard2
+    //     },
+    //     {
+    //         img:BirthdayCard3
+    //     },
+    //     {
+    //         img:BirthdayCard4
+    //     },
+    //     {
+    //         img:HalfSaree1
+    //     },
+    //     {
+    //         img:HalfSaree2
+    //     },
+    //     {
+    //         img:HalfSaree3
+    //     },
+    //     {
+    //         img:HalfSaree4
+    //     },
 
        
-    ]
+    // ]
   return (
     <div className="Our_Collections_Container">
         <div className="Our_Collections_title">
             <h2>OUR COLLECTIONS</h2>
+           
+            {/* <h3>This is vendorId {vendorData}</h3> */}
         </div>
         <div className="Our_Collections_List">
-           {
-            DataList.map((items) => {
-                 return(
-                    <div className="Cards_List">
-                        <img src={items.img} alt="" />
-                    </div>
-                 )
-            })
-           }
+        {
+                vendorData.vendors && vendorData.vendors.map((vendor) => {
+                    return(
+                       <div className="Our_Collections_List">
+                        {
+                            vendor.product.map((item) => {
+                                return(
+                                    <>
+                                    <div className="Cards_List">
+                                       <img src={`${API_URL}/uploads/${item.image}`}  />
+                                       <div className="Cards_List_items">
+                                       <div>{item.productName}</div>
+                                        <div>₹{item.price}</div>
+                                        <div>{item.description}</div>
+                                       </div>
+                                    </div>
+                                    
+                                    </>
+                                )
+                            })
+                        }
+                       </div>
+                    )
+                })
+            }
         </div>
         <div className="button">
              <button>Browse Collections</button>
@@ -80,3 +129,6 @@ const Our_Collections = () => {
 }
 
 export default Our_Collections
+
+
+
