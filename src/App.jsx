6c0292bd4fb368d0b092cd14f6832b953c0ddb55
Sasispecious <sasispecious@gmail.com>
2025-06-30 20:02:38 -0700

    Changes in landscape

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home_Page from './Pages/Home_Page'
 import './App.css'
+import Warning_Page from './Pages/Warning_Page'
 
 const App = () => {
   return (
     <div>
       <Home_Page/>
+      <Warning_Page/>
     </div>
   )
 }
---
+++ src/Pages/Warning_Page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Warning_Page = () => {
+  return (
+    <div className='landscape-warning'>
+
+
+        <p>Please Rotate Your Mobile </p>
+
+    </div>
+
+  )
+}
+
+export default Warning_Page
